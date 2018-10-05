54c7fa5b206c6fe8f9781bdfdd866d0da1b2e135
djuda311 <djuda311@gmail.com>
2018-10-05 18:30:37 -0400

    end of day

--- spec.py
+++ spec.py
@@ -1,8 +1,12 @@
 from scipy import signal
 import matplotlib.pyplot as plt
+from scipy.io import wavfile
 
-sample_rate, samples = wavFile.read('./Audacity/BadBottleBoth.wav')
+sample_rate, samples = wavfile.read('./Audacity/BadBottleBoth.wav')
+print(sample_rate)
 freq, times, spectrogram = signal.spectrogram(samples, sample_rate)
+
+
 
 plt.pcolormesh(times,freq,spectrogram)
 plt.imshow(spectrogram)
